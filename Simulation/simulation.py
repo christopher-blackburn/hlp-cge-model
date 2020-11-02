044e0aa9b95b7de756420f742d61aea813551107
cblackburn-bea <70343506+cblackburn-bea@users.noreply.github.com>
2020-11-02 10:01:35 -0500

    Simulate the Model

--- Simulation/simulation.py
+++ Simulation/simulation.py
@@ -12,12 +12,20 @@ from calibrate import calibrate
 def run_simulation(rho,epsilon,psi):
 
 	# Calibrate the model
+
+	print('--------------------------------------------------------')
+	print('---------------Starting model calibration---------------')
+	print('--------------------------------------------------------')
 	Model = calibrate()
 
 	# Specify elasticities of substitution (this will be a user input)
 	#rho = 2.75
 	#epsilon = 0.5
 	#psi = 2
+
+	print('--------------------------------------------------------')
+	print('---------------Starting model simulation----------------')
+	print('--------------------------------------------------------')
 
 	# Extract the Model Components
 	Pi_f = Model[0]
@@ -34,6 +42,9 @@ def run_simulation(rho,epsilon,psi):
 	Xi_j_mean = Model[11]
 	Xi_j_max = Model[12]
 	usa_domar = Model[13]
+	Xi_j_mean1 = Model[14]
+	Xi_j_mean2 = Model[15]
+	Xi_j_mean3 = Model[16]
 
 
 	# Construct the alpha and eta matrices 
@@ -65,8 +76,22 @@ def run_simulation(rho,epsilon,psi):
 	L_1 = np.nan_to_num(Lambda*eta)
 	L_1 = np.nan_to_num(L_1*(np.eye(168) - alpha))
 
+	# CHN SHOCK ONLY
+	# Average Stringency Index
 	ln_Y_mean = L_1*Xi_j_mean
+
+	# Maximum Stringency Index
 	ln_Y_max = L_1*Xi_j_max
+
+	# MONTHLY IMPACTS
+	# January
+	ln_Y_mo1 = L_1*Xi_j_mean1
+
+	# February
+	ln_Y_mo2 = L_1*Xi_j_mean2
+
+	# March
+	ln_Y_mo3 = L_1*Xi_j_mean3
 
 	print('Successfully simulated log-deviation in output...')
 
@@ -74,6 +99,17 @@ def run_simulation(rho,epsilon,psi):
 	L_2 = (np.eye(168) + (psi/(1+psi))*(np.eye(168) + (np.eye(168) - Pi_f1_kron)*P)*L_1)
 	ln_L_mean = L_2*Xi_j_mean
 	ln_L_max = L_2*Xi_j_max
+
+
+	# MONTHLY IMPACTS -- EMPLOYMENT
+	# January
+	ln_L_mo1 = L_2*Xi_j_mean1
+
+	# February
+	ln_L_mo2 = L_2*Xi_j_mean2
+
+	# March
+	ln_L_mo3 = L_2*Xi_j_mean3
 
 	print('Successfully simulated log-deviation in employment...')
 
@@ -92,10 +128,35 @@ def run_simulation(rho,epsilon,psi):
 
 	gdp_mean_impact = 100*acc_mean['va_cont_j'].sum()
 
-	print('Simulation completed!')
+	# COMPUTE MONTHLY IMPACTS ON GDP
+	d_mo1 = {'usa_eta_j':list(usa_eta_j),'usa_alpha_j':list(usa_alpha_j),'usa_domar_j':usa_domar,'va_imp_max':[x[0] for x in np.array(ln_L_mo1[-56:])[0:]]}
+	d_mo2 = {'usa_eta_j':list(usa_eta_j),'usa_alpha_j':list(usa_alpha_j),'usa_domar_j':usa_domar,'va_imp_max':[x[0] for x in np.array(ln_L_mo2[-56:])[0:]]}
+	d_mo3 = {'usa_eta_j':list(usa_eta_j),'usa_alpha_j':list(usa_alpha_j),'usa_domar_j':usa_domar,'va_imp_max':[x[0] for x in np.array(ln_L_mo3[-56:])[0:]]}
 
-	return [ln_Y_mean,ln_Y_max,ln_L_mean,ln_L_max, gdp_mean_impact,gdp_max_impact]
+	acc_mo1 = pd.DataFrame(d_mo1)
+	acc_mo2 = pd.DataFrame(d_mo2)
+	acc_mo3 = pd.DataFrame(d_mo3)
 
-#results = run_simulation(2.75,0.5,2)
+	acc_mo1['va_cont_j'] = acc_mo1['usa_eta_j']*(1-acc_mo1['usa_alpha_j'])*acc_mo1['usa_domar_j']*acc_mo1['va_imp_max']
+	acc_mo2['va_cont_j'] = acc_mo2['usa_eta_j']*(1-acc_mo2['usa_alpha_j'])*acc_mo2['usa_domar_j']*acc_mo2['va_imp_max']
+	acc_mo3['va_cont_j'] = acc_mo3['usa_eta_j']*(1-acc_mo3['usa_alpha_j'])*acc_mo3['usa_domar_j']*acc_mo3['va_imp_max']
+
+	gdp_impact_mo1 = 100*acc_mo1['va_cont_j'].sum()
+	gdp_impact_mo2 = 100*acc_mo2['va_cont_j'].sum()
+	gdp_impact_mo3 = 100*acc_mo3['va_cont_j'].sum()
+
+	# Q1 IMPACTS BY MONTH
+	gdp_impact_q1 = np.mean([gdp_impact_mo1,gdp_impact_mo2,gdp_impact_mo3])
+
+
+	print('--------------------------------------------------------')
+	print('------------Simulation Successfully Completed-----------')
+	print('--------------------------------------------------------')
+
+	return [ln_Y_mean,ln_Y_max,ln_Y_mo1,ln_Y_mo2,ln_Y_mo3,ln_L_mean,ln_L_max, ln_L_mo1,ln_L_mo2,ln_L_mo3,
+		gdp_mean_impact,gdp_max_impact,gdp_impact_mo1,gdp_impact_mo2,gdp_impact_mo3,gdp_impact_q1]
+
+
+
 
 
